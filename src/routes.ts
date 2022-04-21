b3d7f722ab62c77f32116f4cd8a9c3a4c6070c03
Matt Teran <matthew.a.teran@gmail.com>
2022-04-21 17:44:53 -0500

    makes progress writing query for /reviews/meta but ts is being a huge pain

--- src/routes.ts
+++ src/routes.ts
@@ -2,7 +2,13 @@ import { Router, Request, Response } from "express";
 import { Review } from "./db";
 
 const router = Router();
-
+interface RatingsInt {
+  "1": number;
+  "2": number;
+  "3": number;
+  "4": number;
+  "5": number;
+}
 // routes
 router.get("/reviews", (req: Request, res: Response) => {
   Review.findAll({
@@ -12,22 +18,32 @@ router.get("/reviews", (req: Request, res: Response) => {
   }).then((data) => res.send(data));
 });
 
-// router.get("/reviews/meta", (req: Request, res: Response) => {
-//   Review.findAll({
-//     attributes: ["rating"],
-//     where: {
-//       product_id: 100011,
-//     },
-//   })
-//     .then((data) => {
-//       const ratings: Object = {};
-//       data.forEach((rating: Object) => {
-//         ratings[rating.rating] = 1;
-//       });
-//       res.send(ratings);
-//     })
-//     .catch((err) => console.log(err));
-// });
+router.get("/reviews/meta", (req: Request, res: Response) => {
+  Review.findAll({
+    attributes: ["rating"],
+    where: {
+      product_id: 100011,
+    },
+  })
+    .then((data) => {
+      res.send(
+        data
+          .map((rating) => rating.get("rating"))
+          .reduce((acc: RatingsInt, rating) => {
+            if (rating !== undefined) {
+              if (acc[rating as keyof typeof acc] === undefined) {
+                acc[rating as keyof typeof acc] = 1;
+              } else {
+                acc[rating as keyof typeof acc] = acc[rating as keyof typeof acc] + 1;
+              }
+            }
+            return acc;
+          }, {})
+      );
+      // res.send(data);
+    })
+    .catch((err) => console.log(err));
+});
 
 router.post("/reviews/", (req: Request, res: Response) => {
   res.status(201);
