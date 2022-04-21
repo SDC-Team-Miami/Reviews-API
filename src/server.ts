3fd6d021d09027c182b759e17ca315c58b6c5c58
Matt Teran <matthew.a.teran@gmail.com>
2022-04-20 22:56:52 -0500

    adds prettier

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,15 @@
-import express, { Request, Response } from 'express';
-import morgan from 'morgan';
+import express, { Request, Response } from "express";
+import morgan from "morgan";
 // import db from './db';
 
 const app = express();
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
-app.get('/reviews', (req: Request, res: Response) => {
-  res.send('hello');
+app.get("/reviews", (req: Request, res: Response) => {
+  res.send("hello");
 });
 
 app.listen(8000, () => {
-  console.log('listening on 8000');
+  console.log("listening on 8000");
 });
