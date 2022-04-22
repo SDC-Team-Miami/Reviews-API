23c76c70e2f11673d1739a715abd2f4b602bb5b7
Matt Teran <matthew.a.teran@gmail.com>
2022-04-22 15:09:22 -0500

    queries data for /review/meta

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import AppDataSource from "./data-source";
 import Review from "./entity/Review";
 
 const router = Router();
+const productId = 100011;
 // routes
 router.get("/reviews", (req: Request, res: Response) => {
   AppDataSource.manager
@@ -14,9 +15,17 @@ router.get("/reviews", (req: Request, res: Response) => {
     });
 });
 
-router.get("/reviews/meta", (req: Request, res: Response) => {
-  res.status(200);
-});
+router.get("/reviews/meta", (req: Request, res: Response) =>
+  AppDataSource.manager
+    .find(Review, {
+      where: {
+        product_id: 100011,
+      },
+    })
+    .then((data) => {
+      res.send(data);
+    })
+);
 
 router.post("/reviews/", (req: Request, res: Response) => {
   res.status(201);
