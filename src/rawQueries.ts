55f216cec89a1aa8edee79b1e727cbc143e98129
Matt Teran <matthew.a.teran@gmail.com>
2022-04-24 03:33:06 -0500

    nukes repo

--- src/rawQueries.ts
+++
@@ -1,1 +0,0 @@
-export const getReviewQuery = `SELECT row_to_json(t)FROM(SELECT id AS review_id,rating,summary,recommend,response,body,datetz AS date,reviewer_name,helpfulness,(SELECT json_agg(t)FROM(SELECT id,url FROM photo WHERE photo.review_id=review.id)t)AS photos FROM review WHERE product_id=2)t;`;
