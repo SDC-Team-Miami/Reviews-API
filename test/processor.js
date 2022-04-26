95394429b9ce2c0af1e5b13a90016ce4d32ab6ce
Matt Teran <matthew.a.teran@gmail.com>
2022-04-26 13:50:37 -0500

    moved artillery test files out of repo

--- test/processor.js
+++
@@ -1,9 +0,0 @@
-exports.randomReviewId = (requestParams, context, ee, next) => {
-  context.vars.reviewId = Math.floor(Math.random() * 5774971);
-  return next();
-};
-
-exports.randomProductId = (requestParams, context, ee, next) => {
-  context.vars.productId = Math.floor(Math.random() * 1000011);
-  return next();
-};
